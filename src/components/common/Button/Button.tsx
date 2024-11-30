430299c785cc8a1ba349f1fdd40a43ecbc396c49
mynolog <minholee.log@gmail.com>
2024-12-01 01:32:20 +0900

    ✨ feat: 공통 컴포넌트 Button 구현

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,23 @@
+import type { ReactNode } from 'react'
+
+type ButtonProps = {
+  onClick: () => void
+  children: ReactNode
+  disabled: boolean
+}
+
+const Button = ({ onClick, children, disabled = false }: ButtonProps) => {
+  return (
+    <button
+      className="flex justify-center items-center p-3 w-14 h-14 transition-all
+      duration-200 ease-in-out opacity-80 hover:opacity-100
+       bg-green-700 rounded-full text-white active:scale-95"
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
