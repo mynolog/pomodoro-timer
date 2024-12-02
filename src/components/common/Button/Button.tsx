9f1d177af00582c483947bdde14b54863bfe79d8
mynolog <minholee.log@gmail.com>
2024-12-03 01:20:49 +0900

    ♻️ refactor: 포모도로 타이머 휴식 시간 기능 구현

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -4,14 +4,20 @@ type ButtonProps = {
   onClick: () => void
   children: ReactNode
   disabled?: boolean
+  bgColor?: 'bg-green-700' | 'bg-red-800' | 'bg-gray-900'
 }
 
-const Button = ({ onClick, children, disabled = false }: ButtonProps) => {
+const Button = ({
+  onClick,
+  children,
+  disabled = false,
+  bgColor = 'bg-green-700',
+}: ButtonProps) => {
   return (
     <button
       className={`flex justify-center items-center p-3 w-14 h-14 transition-all
       duration-200 ease-in-out opacity-80 hover:opacity-100
-       bg-green-700 rounded-full text-white active:scale-95 disabled:cursor-not-allowed disabled:bg-gray-800`}
+      ${bgColor} rounded-full text-white active:scale-95 disabled:cursor-not-allowed disabled:bg-gray-400`}
       onClick={onClick}
       disabled={disabled}
     >
--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -1,6 +1,7 @@
 import type { Dispatch, SetStateAction } from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useTimer } from 'react-timer-hook'
+import { MdOutlineFreeBreakfast } from 'react-icons/md'
 import { FaPersonRunning, FaPerson } from 'react-icons/fa6'
 import {
   VscDebugStart,
@@ -29,10 +30,19 @@ const Timer = ({
   pomodoroSeconds,
   setPomodoroSeconds,
 }: TimerProps) => {
+  const [isPomodoro, setIsPomodoro] = useState(true)
   const { seconds, minutes, isRunning, pause, resume, restart } = useTimer({
     expiryTimestamp,
     onExpire: () => {
-      sendNotification('세션 종료', getRandomMessage())
+      if (isPomodoro) {
+        sendNotification('세션 종료', getRandomMessage('pomodoro'))
+        setIsPomodoro(false)
+        initBreakTimestamp()
+      } else {
+        sendNotification('세션 종료', getRandomMessage('break'))
+        setIsPomodoro(true)
+        restartPomodoroTimestamp()
+      }
     },
     autoStart: false,
   })
@@ -44,17 +54,24 @@ const Timer = ({
     restart(newTimestamp, false)
   }, [pomodoroMinutes, pomodoroSeconds, restart])
 
-  const restartTimestamp = () => {
+  const restartPomodoroTimestamp = () => {
+    setIsPomodoro(true)
     const newTimestamp = generateTimestamp(
       pomodoroMinutes * 60 + pomodoroSeconds,
     )
-    restart(newTimestamp, true)
+    restart(newTimestamp)
   }
 
-  const initTimestamp = () => {
+  const initPomodoroTimestamp = () => {
     setPomodoroMinutes(25)
     setPomodoroSeconds(0)
+    setIsPomodoro(true)
     const newTimestamp = generateTimestamp(25 * 60)
+    restart(newTimestamp, false)
+  }
+
+  const initBreakTimestamp = () => {
+    const newTimestamp = generateTimestamp(10 * 60)
     restart(newTimestamp, false)
   }
 
@@ -108,28 +125,36 @@ const Timer = ({
         className={`relative flex items-center gap-2 text-8xl font-degital rounded ${minutes === 0 && seconds <= 10 ? 'text-red-800' : 'text-gray-800'}`}
       >
         <p className="absolute -left-20 text-6xl text-teal-800">
-          {isRunning ? <FaPersonRunning /> : <FaPerson />}
+          {isPomodoro ? (
+            isRunning ? (
+              <FaPersonRunning />
+            ) : (
+              <FaPerson />
+            )
+          ) : (
+            <MdOutlineFreeBreakfast className="text-red-800" />
+          )}
         </p>
         <div className="w-28 h-[176px] flex flex-col gap-1 items-center justify-center">
           <FaAngleUp
-            className={`text-4xl text-green-800 ${isRunning ? 'invisible' : 'visible'}`}
+            className={`text-4xl text-green-800 ${isRunning || !isPomodoro ? 'invisible' : 'visible'}`}
             onClick={handleIncreaseSessionMinute}
           />
           {formatTimeUnit(minutes)}
           <FaAngleDown
-            className={`text-4xl text-green-800 ${isRunning ? 'invisible' : 'visible'}`}
+            className={`text-4xl text-green-800 ${isRunning || !isPomodoro ? 'invisible' : 'visible'}`}
             onClick={handleDecreaseSessionMinute}
           />
         </div>
         <div className={`${isRunning ? 'animate-blink' : ''}`}>:</div>
         <div className="w-28 h-[176px] flex flex-col gap-1 items-center justify-center">
           <FaAngleUp
-            className={`text-4xl text-green-800 ${isRunning ? 'invisible' : 'visible'}`}
+            className={`text-4xl text-green-800 ${isRunning || !isPomodoro ? 'invisible' : 'visible'}`}
             onClick={handleIncreaseSessionSecond}
           />
           {formatTimeUnit(seconds)}
           <FaAngleDown
-            className={`text-4xl text-green-800 ${isRunning ? 'invisible' : 'visible'}`}
+            className={`text-4xl text-green-800 ${isRunning || !isPomodoro ? 'invisible' : 'visible'}`}
             onClick={handleDecreaseSessionSecond}
           />
         </div>
@@ -137,14 +162,20 @@ const Timer = ({
 
       <div className="flex gap-3 text-4xl">
         {isRunning ? (
-          <Button onClick={pause}>{<VscDebugPause />}</Button>
+          <Button bgColor="bg-red-800" onClick={pause}>
+            {<VscDebugPause />}
+          </Button>
         ) : (
           <Button disabled={minutes === 0 && seconds === 0} onClick={resume}>
             {<VscDebugStart />}
           </Button>
         )}
-        <Button onClick={restartTimestamp}>{<VscDebugRerun />}</Button>
-        <Button onClick={initTimestamp}>{<VscDebugRestart />}</Button>
+        <Button bgColor="bg-gray-900" onClick={restartPomodoroTimestamp}>
+          {<VscDebugRerun />}
+        </Button>
+        <Button bgColor="bg-gray-900" onClick={initPomodoroTimestamp}>
+          {<VscDebugRestart />}
+        </Button>
       </div>
     </div>
   )
